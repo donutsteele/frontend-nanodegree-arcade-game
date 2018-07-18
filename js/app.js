afc4c66e113d8dec3ead46ccbcd0d20bf784d657
Ariel Skow <askow01@gmail.com>
2018-07-18 13:53:31 -0500

    Enemy render funtioning

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+    constructor() {
+        this.x = 0;
+        this.y =0;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+    }
+    render (){
+        // Draw the enemy on the screen, required method for game
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    // update(dt){
+
+    // };
+
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,10 +29,8 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
+
 
 // Now write your own player class
 // This class requires an update()
@@ -72,12 +83,15 @@ class Avatar{
       }
     }
 
-    update() {
+    // update(dt) {
 
-    }
+    // }
 }
 
 const player = new Avatar();
+const enemy1 = new Enemy();
+let allEnemies = [];
+allEnemies.push(enemy1);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
--- js/engine.js
+++ js/engine.js
@@ -93,7 +93,7 @@ var Engine = (function(global) {
         // allEnemies.forEach(function(enemy) {
         //     enemy.update(dt);
         // });
-        // player.update();
+       // player.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -150,9 +150,9 @@ var Engine = (function(global) {
          * the render function you have defined.
          */
 
-        // allEnemies.forEach(function(enemy) {
-        //     enemy.render();
-        // });
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
 
          player.render();
     }
